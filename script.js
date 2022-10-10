b4cc3b9c18072152cd9704a62a8aadebe03b0b6b
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-10-10 16:18:48 -0500

    Moved stats variables to the global scope

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+/* Variables to store player wins, losses and ties 
+Stored in the global scope */
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
 
 function playGame() {
-     /* Variables to store player wins, losses and ties */
-     var wins = 0;
-     var losses = 0;
-     var ties = 0;
     /* Users are prompted to enter Rock (R), Paper (P), 
     or Scissors (S) to begin playing. */
     /* Prompts users to enter R P or S. */
@@ -14,7 +16,7 @@ function playGame() {
     var newPlayerChoice = playerChoice.toUpperCase();
     console.log(newPlayerChoice);
 
-    if (playerChoice == null || playerChoice == ""){
+    if (playerChoice == null || playerChoice == "") {
         window.confirm("Ensure to type R, P, or S in the text box shown. Click okay to continue or cancel to end the game.");
         playGame();
     }
@@ -23,8 +25,8 @@ function playGame() {
     /* The player is alerted with the answer choice the application selected. */
     /* The application randomly selects an option from an array */
     var answerChoices = ["R", "P", "S"];
-    
-    var appRandomChoice = answerChoices[Math.floor(Math.random()*answerChoices.length)];
+
+    var appRandomChoice = answerChoices[Math.floor(Math.random() * answerChoices.length)];
     console.log(appRandomChoice);
 
     /* Returns the selected array item as an alert */
@@ -36,17 +38,17 @@ function playGame() {
     determine wins, losses, or ties */
     /* Return the outcome as an alert */
     if (newPlayerChoice == appRandomChoice) {
-        ties++ ;
+        ties++;
         window.alert("It's a tie!");
     } else if (
         (newPlayerChoice === "R" && appRandomChoice === "S") ||
         (newPlayerChoice === "P" && appRandomChoice === "R") ||
         (newPlayerChoice === "S" && appRandomChoice === "P")
     ) {
-        wins++ ;
+        wins++;
         window.alert("You win!");
     } else {
-        losses++ ;
+        losses++;
         window.alert("You lose!");
     }
 
@@ -56,9 +58,14 @@ function playGame() {
 
     /* After clicking okay, the player is asked if they would like to play again.
     Press Okay to play again or cancel to quit. */
-    
+    var rematch = window.confirm("Would you like a rematch? Click okay to play again or cancel to end this session");
     // Call the function to restart the game or end the game
 
-}
+    if (rematch) {
+        playGame();
+    }
 
+};
+
+// Calling the function to run the game as the browser is opened. 
 playGame();
