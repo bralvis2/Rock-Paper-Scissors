e6ab1e77622c7ec23b42962c6b3e3eee32ac818d
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-10-10 15:19:00 -0500

    Pseudo Code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Rock-Paper-Scissors
+
+## Description
+
+
+## How To
+Deployed Application: 
+
+In order to run this project, follow the link provided.
+Once you've landed on the application,
+
+<!-- ![The top of the deploed application](./img/img-1.png)
+![The middle portion of the deployed application](./img/img-2.png)
+![Mobil version](./img/img-3.png) -->
+
+## Credits
+I used refrence materials from the UT Full Stack curriculum. 
+
+[W3 Schools Array Methods](https://www.w3schools.com/js/js_array_methods.asp)
+
+[MDM Array Methods](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
---
+++ script.js
@@ -0,0 +1,23 @@
+/* Users are prompted to enter Rock (R), Paper (P), or Scissors (S)
+to begin playing. */
+    /* Prompts users to enter R P or C. */
+    /* Application should accept lowercase letters too */
+     /* Store user input as a variable */
+
+
+/* The player is alerted with the answer choice the application selected. */
+     /* The application randomly selects an option from an array */
+      /* Returns the selected array item as an alert */
+
+/* After the player clicks okay, the player is alerted if they won, lost, or tied */
+     /* compare the player choice variable and the application choice array item to
+     determine wins, losses, or ties */
+      /* Return the outcome as an alert */
+
+/* Then the player is alerted their statics. of wins, losses, and ties. */
+     /* Variables to store player wins, losses and ties */
+      /* Variables are returned in an alert */
+
+/* After clicking okay, the player is asked if they would like to play again.
+Press Okay to play again or cancel to quit. */
+     /* Call the function to restart the game or end the game */
