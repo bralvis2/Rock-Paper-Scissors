da4d0444c914d6ae35bb1e0b194042964d820166
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-10-10 15:39:03 -0500

    Stored User Variable and Random Choice

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-/* Users are prompted to enter Rock (R), Paper (P), or Scissors (S)
-to begin playing. */
-    /* Prompts users to enter R P or C. */
+
+function playGame() {
+    /* Users are prompted to enter Rock (R), Paper (P), 
+    or Scissors (S) to begin playing. */
+    /* Prompts users to enter R P or S. */
+    /* Store user input as a variable */
+    var playerChoice = window.prompt("To begin playing, enter R, P, or S below");
+
     /* Application should accept lowercase letters too */
-     /* Store user input as a variable */
+    var newPlayerChoice = playerChoice.toUpperCase();
+    console.log(newPlayerChoice);
 
 
-/* The player is alerted with the answer choice the application selected. */
-     /* The application randomly selects an option from an array */
-      /* Returns the selected array item as an alert */
+    /* The player is alerted with the answer choice the application selected. */
+    /* The application randomly selects an option from an array */
+    var answerChoices = ["Rock", "Paper", "Scissors"];
+    
+    var appRandomChoice = answerChoices[Math.floor(Math.random()*answerChoices.length)];
+    console.log(appRandomChoice);
+    /* Returns the selected array item as an alert */
 
-/* After the player clicks okay, the player is alerted if they won, lost, or tied */
-     /* compare the player choice variable and the application choice array item to
-     determine wins, losses, or ties */
-      /* Return the outcome as an alert */
+    var appChoice = window.alert("The computer has selected " + appRandomChoice);
 
-/* Then the player is alerted their statics. of wins, losses, and ties. */
-     /* Variables to store player wins, losses and ties */
-      /* Variables are returned in an alert */
 
-/* After clicking okay, the player is asked if they would like to play again.
-Press Okay to play again or cancel to quit. */
-     /* Call the function to restart the game or end the game */
+    /* After the player clicks okay, the player is alerted if they won, lost, or tied */
+    /* compare the player choice variable and the application choice array item to
+    determine wins, losses, or ties */
+    /* Return the outcome as an alert */
+
+    /* Then the player is alerted their statics. of wins, losses, and ties. */
+    /* Variables to store player wins, losses and ties */
+    /* Variables are returned in an alert */
+
+    /* After clicking okay, the player is asked if they would like to play again.
+    Press Okay to play again or cancel to quit. */
+    // Call the function to restart the game or end the game
+
+}
+
+playGame();
